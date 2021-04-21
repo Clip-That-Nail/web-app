a10f6d22962c2dc1f05f9c1ab0b3959310aaa471
EmiliaMlak <emiliamlak@gmail.com>
2021-04-21 19:39:36 +0100

    created folder architecture for the project and started homepage component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <h1 className='title'>Clip That Nail</h1>
+            <button></button>
+        </div>
+    </div>
+);
+
+export default HomePage;
