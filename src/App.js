a770495d879dd4c0f03ff1a54c6833fac8fd05d8
EmiliaMlak <emiliamlak@gmail.com>
2021-04-26 20:09:23 +0100

    installing material-ui packages, creating very basic header component, subject to change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Header></Header>
     </div>
   );
 }
